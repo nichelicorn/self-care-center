2171811ede0606ec87245c448c729c95c7bf6757
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-18 14:00:10 -0700

    Add pseudocode

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// ⤵ query selectors
+
+
+// ⤵ data model
+
+
+// ⤵ event listeners
+
+
+// ⤵ functions
+
+
+// 1a) write a function that will generate a random index number
+//  b) write a function that will pull a value from the appropriate array, based on the random index generated
+// --- should the generate random function call the message function with the index argument passed in? yes, I like that idea
+
+
+
+
+
 // Iteration 1: Minimum Viable Product (MVP) - Add Random Affirmation and Mantra feature
 // When a user selects a message option and then clicks the “Receive Message” button, the user sees a random message from the list of possible messages for that category
 // When the message appears, the mediation icon disappears from the message area
@@ -7,9 +27,6 @@
 //  2) the meditation icon will disappear from view
 //  3) in its place, the random message will be displayed to the user
 
-// 1a) write a function that will generate a random index number
-//  b) write a function that will pull a value from the appropriate array, based on the random index generated
-// --- should the generate random function call the message function with the index argument passed in? yes, I like that idea
 
 // 2a) write a function that will hide the mediation icon
 //  b) write a function that will unhide the message display element
