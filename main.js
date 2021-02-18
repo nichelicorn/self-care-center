b1c150b7e58b7dfdd811b4c483b3fbb6b90cf4fc
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-18 13:52:51 -0700

    Add pseudocode

---
+++ main.js
@@ -0,0 +1,12 @@
+// Iteration 1: Minimum Viable Product (MVP) - Add Random Affirmation and Mantra feature
+// When a user selects a message option and then clicks the “Receive Message” button, the user sees a random message from the list of possible messages for that category
+// When the message appears, the mediation icon disappears from the message area
+
+// goal : when a radio button is selected and the `Receive Message` button is clicked, several things will happen:
+//  1) a random message will be generated based on the user input
+//  2) the meditation icon will disappear from view
+//  3) in its place, the random message will be displayed to the user
+
+// 1a) write a function that will generate a random index number
+//  b) write a function that will pull a value from the appropriate array, based on the random index generated
+//  c) write a function that will replace the icon with an innerText 
